8740bfb678e295b8eb7db8edcc51bb5325ecc3d4
AOKI Yuuto <yuto.aoki@japan-d2.com>
2020-02-18 22:36:45 +0900

    refactor: convert eslint config js to yaml

---
+++ .eslintrc.parser.js
@@ -0,0 +1,9 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: false },
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+}
