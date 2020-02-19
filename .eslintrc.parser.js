4dca84f1feb257972932f0b62abb333e14e7ba0d
AOKI Yuuto <yuto.aoki@japan-d2.com>
2020-02-19 10:23:24 +0900

    refactor: merge parser config to .eslintrc.yml

--- .eslintrc.parser.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: { jsx: false },
-    project: ['./tsconfig.json'],
-    tsconfigRootDir: __dirname,
-  },
-}
